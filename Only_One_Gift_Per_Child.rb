b7f28a5790174f1c7942c03fc4884c4232cc9424
sanjsanj <sanjsanj@hotmail.com>
2015-03-05 13:46:33 +0000

    Start work on Only_One_Gift_Per_Child

---
+++ Only_One_Gift_Per_Child.rb
@@ -0,0 +1,31 @@
+# question
+# Santa is handing out gifts in the kindergarten. Many toddlers are around there and everyone should have the opportunity to have a seat on Santa's lap. But there's Peter, a 5 year old boy, who is a bit naughty. He tries to get two gifts. After he got the first one, he lines up again in the queue of children.
+#
+# But fortunately Santa is not alone. His elves keep a list with the names of the children, which already were on Santa's lap. We know, that each child name is unique. If a child tries to get a second gift, the elves will tell Santa.
+#
+# OK, let's help Santa and his elves with a simple function handOutGift() (hand_out_gift in Ruby) which takes the name of the next child. If this child already got a gift, it must throw an error in order to remind Santa.
+#
+# Ruby Example
+#
+# hand_out_gift("Peter");
+# hand_out_gift("Alison");
+# hand_out_gift("John");
+# hand_out_gift("Maria");
+# hand_out_gift("Peter"); // <-- must throw an error
+
+def hand_out_gift(name)
+  ary = Array.new
+  if ary.include?(name)
+    raise "//"
+  else
+    ary.push(name)
+    p true
+  end
+end
+
+# tests
+hand_out_gift("Peter")
+hand_out_gift("Alison")
+hand_out_gift("John")
+hand_out_gift("Maria")
+hand_out_gift("Peter")
