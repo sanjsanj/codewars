14c00a2d21ed1b9df9ed4c48a2f6f69b98830aea
sanjsanj <sanjsanj@hotmail.com>
2015-03-06 08:42:34 +0000

    Complete Populate_hash_with_array_keys_and_default_value

---
+++ Populate_hash_with_array_keys_and_default_value.rb
@@ -0,0 +1,9 @@
+# question
+# Complete the solution so that it takes an array of keys and a default value and returns a hash with all keys set to the default value.
+#
+# Example:
+# solution([:draft, :completed], 0) # should return {draft: 0, completed: 0}
+
+def solution(keys, default_value)
+  Hash[keys.product([default_value])]
+end
