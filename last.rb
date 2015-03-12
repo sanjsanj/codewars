1e8115ff836de8d304efaf73d2f2ce32f4f5ffae
sanjsanj <sanjsanj@hotmail.com>
2015-03-12 11:00:43 +0000

    Progress with last

--- last.rb
+++ last.rb
@@ -1,10 +1,13 @@
-def last(block)
-  block[block.length - 1] if block.class == Array
-  block[block.length - 1] if block.class == String
-  block.join("")
+def last(stuff)
+  if stuff.is_a?(String)
+    puts "string"
+  elsif stuff.is_a?(Array)
+    puts "array"
+  else
+    stuff[stuff.length - 1]
+  end
 end
 
-
-last([1,2,3,4,5])
-last("abcde")
-last(1, "b", 3, "d", 5)
+puts last([1,2,3,4,5])
+puts last("abcde")
+puts last(1, "b", 3, "d", 5)
